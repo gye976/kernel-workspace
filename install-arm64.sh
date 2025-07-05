e9d4ee6da036468cbfd1401b260b9cf5bbc7e6c8
Gyeyoung Baek <gye976@gmail.com>
2025-07-05 20:51:54 +0900

    amd64 -> install-arm64

--- install-arm64.sh
+++ install-arm64.sh
@@ -1,7 +1,7 @@
 source util.sh
 set_kernel_tree $1
 
-sudo cp ${OUTPUT}/arch/x86/boot/bzImage . && \
+sudo cp ${OUTPUT}/arch/arm64/boot/Image Image && \
 sudo mount rootfs.ext2 mnt && \
 sudo rm -rf mnt/lib/modules && \
 sudo ./make.sh ${TARGET} modules_install INSTALL_MOD_PATH=$(pwd)/mnt && \
